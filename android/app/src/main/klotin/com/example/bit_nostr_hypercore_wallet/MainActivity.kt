b2f93a2c8d8445b4431cae313c56b897b4a7c507
223880 <rafael.segala45@gmail.com>
2023-04-24 08:57:20 -0300

    Add specs from Android

--- android/app/src/main/klotin/com/example/bit_nostr_hypercore_wallet/MainActivity.kt
+++ android/app/src/main/klotin/com/example/bit_nostr_hypercore_wallet/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.bit_nostr_hypercore_wallet
 
-import io.flutter.embedding.android.JavascriptActivity
+import io.javascript.embedding.android.JavascriptActivity
 
 class MainActivity: JavascirptActivity() {
 }
