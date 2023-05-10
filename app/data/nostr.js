9ad6495e7261c31edf6444f5efe9bb00f76a60ed
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-10 16:23:39 -0300

    Add generate Nostr keys vanilla

--- app/data/nostr.js
+++ app/data/nostr.js
@@ -1,7 +1,115 @@
 // Load the necessary libraries
 const bip39 = require('bip39');
 const Nostr = require('nostr-js');
+import { generatePrivateKey, getPublicKey } from "nostr-tools";
 
+ const [privateKey, setPrivateKey] = React.useState("");
+  const [publicKey, setPublicKey] = React.useState("");
+
+  const GeneratePair = () => {
+    let sk = generatePrivateKey();
+    setPrivateKey(sk);
+    let pk = getPublicKey(sk);
+    setPublicKey(pk);
+  };
+
+  useEffect(() => {
+    GeneratePair();
+  }, []);
+
+  const [prefix, setPrefix] = useState("be");
+  const handleChangePrefix = (event) => {
+    setPrefix(event.target.value);
+    setDoneGenerating(false);
+  };
+
+  const [addressesGenerated, setAddressesGenerated] = useState(0);
+  const [loading, setLoading] = useState("false");
+
+  const VanityPairSlow = async () => {
+    setAddressesGenerated(0);
+    let svk = generatePrivateKey();
+    let pvk = getPublicKey(svk);
+    let i = 0;
+    while (pvk.substring(0, prefix.length) !== prefix) {
+      svk = generatePrivateKey();
+      pvk = getPublicKey(svk);
+      i++;
+      setLoading("true");
+      if (i % 100 === 0) {
+        setAddressesGenerated(i);
+        await new Promise((r) => setTimeout(r, 1));
+      }
+    }
+    setLoading("false");
+    setPrivateKey(svk);
+    setPublicKey(pvk);
+    setAddressesGenerated(i);
+    setDoneGenerating(true);
+  };
+  const VanityPair = async () => {
+    setLoading("true");
+    setAddressesGenerated(0);
+    let svk = generatePrivateKey();
+    let pvk = getPublicKey(svk);
+    let i = 0;
+    while (pvk.substring(0, prefix.length) !== prefix) {
+      svk = generatePrivateKey();
+      pvk = getPublicKey(svk);
+      i++;
+      if (i % 3000 === 0) {
+        await new Promise((r) => setTimeout(r, 0.01));
+        setAddressesGenerated(i);
+      }
+    }
+    setLoading("false");
+    setPrivateKey(svk);
+    setPublicKey(pvk);
+    setAddressesGenerated(i);
+    setDoneGenerating(true);
+  };
+
+  const [checked, setChecked] = React.useState(true);
+  const handleChangeChecked = (event) => {
+    setChecked(event.target.checked);
+    setAddressesGenerated(0);
+    setDoneGenerating(false);
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [highlight, setHighlight] = React.useState("");
+  useEffect(() => {
+    const HighlightPref = (prefix, pubkey) => {
+      let pubkeySub = pubkey.substring(0, prefix.length);
+      let prefixSub = pubkey.substring(prefix.length, pubkey.length);
+      return isMobile ? (
+        <Stack direction="row" justifyContent={"space-around"} sx={{ px: 2 }}>
+          <Box sx={{ color: "#44ff00" }}>
+            <Typography>{pubkeySub}</Typography>
+          </Box>
+          <Box sx={{ overflowWrap: "break-word", width: "100%" }}>
+            <Typography align={"right"}>{prefixSub}</Typography>
+          </Box>
+        </Stack>
+      ) : (
+        <Stack direction="row">
+          <Box sx={{ color: "#44ff00" }}>
+            <Typography variant="h6">{pubkeySub}</Typography>
+          </Box>
+
+          <Typography variant="h6" align={"right"}>
+            {prefixSub}
+          </Typography>
+        </Stack>
+      );
+    };
+    setHighlight(HighlightPref(prefix, publicKey));
+  }, [prefix, publicKey]);
+
+  const [doneGenerating, setDoneGenerating] = React.useState(false);
 // Set up the Nostr client
 const nostrClient = new Nostr('wss://relay.damus.io');
 
