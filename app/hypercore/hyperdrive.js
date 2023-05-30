51becd3186f7c7c9cdd2ed727d6097189fa37ca4
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-30 14:50:06 -0300

    Update hyperdrive.js

--- app/hypercore/hyperdrive.js
+++ app/hypercore/hyperdrive.js
@@ -1,1 +1,33 @@
 const hyperdrive = require('@holepunch/hyperdrive')
+const Corestore = require('corestore')
+
+const store = new Corestore('./storage')
+const drive = new Hyperdrive(store)
+
+await drive.put('/blob.txt', Buffer.from('example'))
+await drive.put('/images/logo.png', Buffer.from('..'))
+await drive.put('/images/old-logo.png', Buffer.from('..'))
+
+const buffer = await drive.get('/blob.txt')
+console.log(buffer) // => <Buffer ..> "example"
+
+const entry = await drive.entry('/blob.txt')
+console.log(entry) // => { seq, key, value: { executable, linkname, blob, metadata } }
+
+await drive.del('/images/old-logo.png')
+
+await drive.symlink('/images/logo.shortcut', '/images/logo.png')
+
+for await (const file of drive.list('/images')) {
+  console.log('list', file) // => { key, value }
+}
+
+const rs = drive.createReadStream('/blob.txt')
+for await (const chunk of rs) {
+  console.log('rs', chunk) // => <Buffer ..>
+}
+
+const ws = drive.createWriteStream('/blob.txt')
+ws.write('new example')
+ws.end()
+ws.once('close', () => console.log('file saved'))
