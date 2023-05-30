d4d452a3d9cb10a16634529e32b65c4b56fd3927
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-30 14:50:36 -0300

    Update hyperswarm.js

--- app/hypercore/hyperswarm.js
+++ app/hypercore/hyperswarm.js
@@ -1,1 +1,24 @@
 const hyperswarm = require('@holepunch/hyperswarm')
+const swarm1 = new Hyperswarm()
+const swarm2 = new Hyperswarm()
+
+swarm1.on('connection', (conn, info) => {
+  // swarm1 will receive server connections
+  conn.write('this is a server connection')
+  conn.end()
+})
+
+swarm2.on('connection', (conn, info) => {
+  conn.on('data', data => console.log('client got message:', data.toString()))
+})
+
+const topic = Buffer.alloc(32).fill('hello world') // A topic must be 32 bytes
+const discovery = swarm1.join(topic, { server: true, client: false })
+await discovery.flushed() // Waits for the topic to be fully announced on the DHT
+
+swarm2.join(topic, { server: false, client: true })
+await swarm2.flush() // Waits for the swarm to connect to pending peers.
+
+// After this point, both client and server should have connections
+
+
