d5a8acc62fee618619ef0b210e98a433a7e818bd
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-19 12:06:37 -0300

    Update contact_list.js

--- app/pages/contact_list.js
+++ app/pages/contact_list.js
@@ -1,8 +1,12 @@
 const nostr = require('nostr-tools');
 const tbd = require('web5-js');
+const {PubKey} = require('nostr-tools');
 
 const contact_list = {
-  npub:
-  id:
+  npub: npub
+  relay: relay
+  
+ conts pubkey = {
+  
   
   
