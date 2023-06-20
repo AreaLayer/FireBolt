bdfb938f1bd08482d74d6f06c525081ff4d008ac
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-06-20 07:51:25 -0300

    Update miniscript.js

--- app/bitcoin/miniscript.js
+++ app/bitcoin/miniscript.js
@@ -3,3 +3,6 @@ const { compilePolicy } = require('@bitcoinerlab/miniscript');
 const policy = 'or(and(pk(A),older(8640)),pk(B))';
 
 const { miniscript, asm, issane } = compilePolicy(policy);
+
+// Initial miniscript
+async miniscript 
