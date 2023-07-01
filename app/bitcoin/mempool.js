98d2ad8abd7e551e30c62b7aae43e208096440eb
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-07-01 10:57:45 -0300

    Create mempool.js

---
+++ app/bitcoin/mempool.js
@@ -0,0 +1,8 @@
+import mempoolJS from '@mempool/mempool.js';
+
+// default mempool.space endpoints
+const { bitcoin } = mempoolJS();
+
+// (optional) your custom endpoints
+const { bitcoin } = mempoolJS({
+  hostname: 'mempool.space', 
