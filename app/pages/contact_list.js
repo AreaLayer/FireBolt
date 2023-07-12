8488ecf646594e3c70b074ed29247c4bf8a34fd5
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-07-12 08:32:15 -0300

    Update contact_list.js

--- app/pages/contact_list.js
+++ app/pages/contact_list.js
@@ -2,9 +2,38 @@ const nostr = require('nostr-tools');
 const tbd = require('web5-js');
 const {PubKey} = require('nostr-tools');
 
-const contact_list = {
-  npub: npub
-  relay: relay
+const contacts = [
+  {
+    name: 'John Doe',
+    nostr: {
+      npubKey: 'Npb12', // Example NPub key
+      relay: 'https://relay.damus.io/' // Example relay URL
+    },
+    web5: {
+      did: 'did:ion:EiD7Qq48bOQ16zH_hg8U4...', // Example DID
+      explorer: 'https://identity.foundation/ion/explorer/did:ion:EiD7Qq48bOQ16zH_hg8U4...' // Example explorer URL
+    }
+  },
+  {
+    name: 'Jane Smith',
+    nostr: {
+      npubKey: 'Npb12', // Example NPub key
+      relay: 'https://relay.damus.io/' // Example relay URL
+    },
+    web5: {
+      did: 'did:ion:EiD7Qq48bOQ16zH_hg8U4...', // Example DID
+      explorer: 'https://identity.foundation/ion/explorer/did:ion:EiD7Qq48bOQ16zH_hg8U4...' // Example explorer URL
+    }
+  }
+];
+
+// Accessing Contact Information
+console.log('Contact 1 Name:', contacts[0].name);
+console.log('Contact 1 Nostr NPub Key:', contacts[0].nostr.npubKey);
+console.log('Contact 1 Nostr Relay:', contacts[0].nostr.relay);
+console.log('Contact 1 Web 5 DID:', contacts[0].web5.did);
+console.log('Contact 1 Web 5 Explorer:', contacts[0].web5.explorer);
+
     
   
   
