df444fdb311d5ab3876af9e02a45e9f1973579fb
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-28 06:27:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
+const App = () => {
+  useEffect(() => {
+    // Add any necessary initialization code here
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Your Mobile App</Text>
+      {/* Add your components and UI elements here */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  // Add more styles as needed
+});
+
+export default App;
