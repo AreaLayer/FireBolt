fcaf0e099dc21c6298af15650531af8c7e1b3bd0
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-08 06:31:09 -0300

    Update sw.js

--- pwa/img/sw.js
+++ pwa/img/sw.js
@@ -1,1 +1,35 @@
+const CACHE_NAME = 'wallet-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  // Add more URLs to cache as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+            .map(cacheName => caches.delete(cacheName))
+        );
+      })
+  );
+});
 
