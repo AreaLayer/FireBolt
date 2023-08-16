38a8c1eb81172536653f72fac0cc25bdd5f75541
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-16 07:20:30 -0300

    Update lightning.js

--- app/server/lightning.js
+++ app/server/lightning.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
+const app = express();
+const port = 3000; // Choose a port number
+
+app.use(bodyParser.json());
+
+// Define the base URL of your LND server
+const lndBaseUrl = 'http://localhost:8080'; // Replace with your LND server's URL
+
+// Example route to get information about your node
+app.get('/getinfo', async (req, res) => {
+    try {
+        const response = await fetch(`${lndBaseUrl}/v1/getinfo`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'An error occurred while fetching data' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
