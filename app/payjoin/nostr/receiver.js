c1dc92f5ab2b6da25848d285d5f7e30132f9b471
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-22 09:38:33 -0300

    Update receiver.js

--- app/payjoin/nostr/receiver.js
+++ app/payjoin/nostr/receiver.js
@@ -39,29 +39,3 @@ async function generatePSBT(input, output, senderSignedPsbt) {
     });
 
     return psbt.toBase64();
-}
-
-    async function handleFormSubmit(event) {
-      event.preventDefault();
-
-      const input = document.getElementById('input').value;
-      const output = document.getElementById('output').value;
-      const senderSignedPsbt = document.getElementById('psbt').value;
-      const psbtBase64 = await generatePSBT(input, output, senderSignedPsbt);
-
-      const updatedPsbtTextarea = document.getElementById('psbt');
-      updatedPsbtTextarea.value = psbtBase64;
-
-      alert('\n Successfully updated PSBT with new input and output');
-
-    }
-
-
-    function resetFormFields() {
-      document.getElementById('input').value = '';
-      document.getElementById('output').value = '';
-      document.getElementById('psbt').value = '';
-    }
-
-    document.getElementById('form').addEventListener('submit', handleFormSubmit);
-    window.addEventListener('load', resetFormFields);
