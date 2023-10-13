6806bff9637fc335d83ec04da59be087fdce1754
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-13 11:32:13 -0300

    Update main.storyboard.swift

--- ios/ui/main.storyboard.swift
+++ ios/ui/main.storyboard.swift
@@ -1,1 +1,16 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        titleLabel.text = "Button Tapped!"
+    }
+}
 
