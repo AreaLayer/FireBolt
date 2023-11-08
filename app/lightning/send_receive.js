ed93d75d58a998bb4843000bf72cf311dd98b152
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-08 07:11:39 -0300

    Update send_receive.js

--- app/lightning/send_receive.js
+++ app/lightning/send_receive.js
@@ -65,48 +65,48 @@ const createInvoice = () => {
   });
 };
 // Taproot Channels
+cosnt TaprootChannels = 
+this.postRequest(
+  data.simpleTaprootChannel
+    ? {
+        private: data.privateChannel,
+        scid_alias: data.scidAlias,
+        local_funding_amount: data.local_funding_amount,
+        min_confs: data.min_confs,
+        node_pubkey_string: data.node_pubkey_string,
+        sat_per_vbyte: data.sat_per_vbyte,
+        spend_unconfirmed: data.spend_unconfirmed,
+        fund_max: data.fundMax,
+        outpoints: data.utxos
+          ? data.utxos.map((utxo) => {
+              const [txid_str, output_index] = utxo.split(':');
+              return {
+                txid_str,
+                output_index: Number(output_index),
+              };
+            })
+          : undefined,
+        commitment_type: 'SIMPLE_TAPROOT',
+      }
+    : {
+        private: data.privateChannel,
+        scid_alias: data.scidAlias,
+        local_funding_amount: data.local_funding_amount,
+        min_confs: data.min_confs,
+        node_pubkey_string: data.node_pubkey_string,
+        sat_per_vbyte: data.sat_per_vbyte,
+        spend_unconfirmed: data.spend_unconfirmed,
+        fund_max: data.fundMax,
+        outpoints: data.utxos
+          ? data.utxos.map((utxo) => {
+              const [txid_str, output_index] = utxo.split(':');
+              return {
+                txid_str,
+                output_index: Number(output_index),
+              };
+            })
+          : undefined,
+      }
+);
 
- this.postRequest(
-            data.simpleTaprootChannel
-                ? {
-                      private: data.privateChannel,
-                      scid_alias: data.scidAlias,
-                      local_funding_amount: data.local_funding_amount,
-                      min_confs: data.min_confs,
-                      node_pubkey_string: data.node_pubkey_string,
-                      sat_per_vbyte: data.sat_per_vbyte,
-                      spend_unconfirmed: data.spend_unconfirmed,
-                      fund_max: data.fundMax,
-                      outpoints: data.utxos
-                          ? data.utxos.map((utxo: string) => {
-                                const [txid_str, output_index] =
-                                    utxo.split(':');
-                                return {
-                                    txid_str,
-                                    output_index: Number(output_index)
-                                };
-                            })
-                          : undefined,
-                      commitment_type: 'SIMPLE_TAPROOT'
-                  }
-                : {
-                      private: data.privateChannel,
-                      scid_alias: data.scidAlias,
-                      local_funding_amount: data.local_funding_amount,
-                      min_confs: data.min_confs,
-                      node_pubkey_string: data.node_pubkey_string,
-                      sat_per_vbyte: data.sat_per_vbyte,
-                      spend_unconfirmed: data.spend_unconfirmed,
-                      fund_max: data.fundMax,
-                      outpoints: data.utxos
-                          ? data.utxos.map((utxo: string) => {
-                                const [txid_str, output_index] =
-                                    utxo.split(':');
-                                return {
-                                    txid_str,
-                                    output_index: Number(output_index)
-                                };
-                            })
-                          : undefined
-                  }
         );
