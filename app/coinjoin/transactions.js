6e8c4016bbd72dd6298a1c672f0f83d7c74cf21f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-12-29 06:06:29 -0300

    Update transactions.js

--- app/coinjoin/transactions.js
+++ app/coinjoin/transactions.js
@@ -3,3 +3,10 @@ const bitcoin = require('bitcoinjs-lib');
 async function trasaction = {
   utxo: uxto,
   input: input,
+  output: output,
+
+
+async function validation = {
+
+
+async function lightning = {
