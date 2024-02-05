7dfa80c8a223f4d85941a1e4cd3084ed2992a615
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-02-05 11:50:04 -0300

    Update core.js

--- app/payjoin/core.js
+++ app/payjoin/core.js
@@ -1,4 +1,5 @@
 const payjoin = require('payjoin-client');
+const {client, request, wallet} = require('payjoin-client');
 const bitcoin = require('bitcoinjs-lib');
 const network = bitcoin.networks.testnet;
 
