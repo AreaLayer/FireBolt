885cd83677e62c091c5d68f6a3840a1fa3b85bd9
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-03-04 14:06:46 -0300

    Update payjoinservice.swift

--- ios/app/payjoinservice.swift
+++ ios/app/payjoinservice.swift
@@ -5,6 +5,7 @@ class PayJoinService {
 
     func initiatePayJoinTransaction(transactionData: Any) -> Any {
         // Implement function to initiate PayJoin transaction
+        class utxo
         let response = payJoinClient.post(transactionData)
         return response.data
     }
