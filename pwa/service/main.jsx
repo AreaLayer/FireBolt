0cadfad52de566a6c9d999b9ca5e8e709a36538b
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-03-13 13:07:26 -0300

    Delete pwa directory

--- pwa/service/main.jsx
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
-}
