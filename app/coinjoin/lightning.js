b4204cb74ef3ae9f6edbde4f30efa28d01366c0c
223880 <rafael.segala45@gmail.com>
2024-03-23 07:56:07 -0300

    Fix LN payment <> CJ/UTXO

--- app/coinjoin/lightning.js
+++ app/coinjoin/lightning.js
@@ -1,18 +1,7 @@
-const {PaymentHash, Payment, TXID, UTXO} from 'react-native-breez';
+import {PaymentHash, TXID, UTXO} from 'react-native-breez';
 
 const Payment = {
-Invoice: String;
-PaymentMethodChangeEvent: String;
-
-}
-const hash = {
-
-}
-
-const txid = {
-    
-}
-
-const uxto = {
-
-}
+  PaymentHash,
+  TXID,
+  UTXO
+};
